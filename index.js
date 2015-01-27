067fa90fdb7fed1f31d36a78ad835fce1a46532d
ravenjohn <ravenjohn@users.noreply.github.com>
2015-01-27 16:22:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-module.exports = function (allowed_origins) {
+module.exports = function (allowed_origins, allowed_headers) {
 
     return function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', allowed_origins);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, X-ACCESS-TOKEN', 'Authentication');
+        res.setHeader('Access-Control-Allow-Headers', allowed_headers);
 
         if (req.method === 'OPTIONS') {
             return res.status(200)
