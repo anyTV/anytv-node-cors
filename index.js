b9bbed783f93bd1fb73a1e10ed5e67d3b730f65b
Gian Crescini Santillan <giansantillan18@gmail.com>
2015-01-27 15:52:25 +0800

    Added Authentication in allowed headers.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function (allowed_origins) {
     return function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', allowed_origins);
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, X-ACCESS-TOKEN');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, X-ACCESS-TOKEN', 'Authentication');
 
         if (req.method === 'OPTIONS') {
             return res.status(200)
