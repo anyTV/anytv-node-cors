8284e8199ed15eb844143f1dfc778fb631b562cc
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-01-27 17:34:10 +0800

    Changed config param

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-module.exports = function (allowed_origins, allowed_headers) {
+module.exports = function (config) {
 
     return function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', allowed_origins);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', allowed_headers);
+        res.setHeader('Access-Control-Allow-Origin', config.allowed_origins);
+        res.setHeader('Access-Control-Allow-Methods', config.allowed_methods);
+        res.setHeader('Access-Control-Allow-Headers', config.allowed_headers);
 
         if (req.method === 'OPTIONS') {
             return res.status(200)
