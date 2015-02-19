90b0f5d6389935bed6f931c271c1ffdf6ee589ec
Raven Lagrimas <rjlagrimas08@gmail.com>
2015-02-19 11:49:46 +0800

    Added default value

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+'use strict';
 
 module.exports = function (config) {
+    config = config || {};
 
     return function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', config.allowed_origins);
-        res.setHeader('Access-Control-Allow-Methods', config.allowed_methods);
-        res.setHeader('Access-Control-Allow-Headers', config.allowed_headers);
+        if (config.allowed_origins) {
+            res.setHeader('Access-Control-Allow-Origin', config.allowed_origins);
+        }
+
+        if (config.allowed_methods) {
+            res.setHeader('Access-Control-Allow-Methods', config.allowed_methods);
+        }
+
+        if (config.allowed_headers) {
+            res.setHeader('Access-Control-Allow-Headers', config.allowed_headers);
+        }
 
         if (req.method === 'OPTIONS') {
             return res.status(200)
