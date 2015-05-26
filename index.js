5c601946edfb5ad65e2f687cfb02a72ace17ea27
aldrin bautista <adinbautista@gmail.com>
2015-05-26 12:42:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ module.exports = function (config) {
     config = config || {};
 
     return function (req, res, next) {
-        if (config.allowed_origins) {
-            res.setHeader('Access-Control-Allow-Origin', config.allowed_origins);
+
+        if (config.allowed_origins || config.allowed_origins_list) {
+            if (config.allowed_origins_list) {
+                for (var i=0; i<config.allowed_origins_list.length; i++) {
+                    if (req.headers.origin && req.headers.origin.match(config.allowed_origins_list[i])) {
+                        res.setHeader('Access-Control-Allow-Credentials', config.allow_credentials);
+                        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+                        break;
+                    }
+                }
+
+                if (i==config.allowed_origins_list.length || !req.headers.origin) {
+                    res.setHeader('Access-Control-Allow-Origin', config.allowed_origins);
+                }
+            } else {
+                res.setHeader('Access-Control-Allow-Origin', config.allowed_origins);
+            }
         }
 
         if (config.allowed_methods) {
